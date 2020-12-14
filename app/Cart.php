503dd2007a584f195e92098788fbd936b8b23334
putradandy619@gmail.com <putradandy619@gmail.com>
2020-12-14 16:12:47 +0700

    tes

---
+++ app/Cart.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    protected $fillable = [
+        'user_id', 'product_id', 'qty'
+    ];
+
+    Public function Product()
+    {
+	    return $this->belongsTo('App\Product');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
+
+
